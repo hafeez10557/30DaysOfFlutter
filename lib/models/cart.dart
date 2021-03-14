33e0b16d2c6b77732bbc1ede4b25f2f3bbcde386
Abdul hafeez <a.hafeez10557@gmail.com>
2021-03-14 14:24:00 +0500

    day 27 Completed

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,3 +1,6 @@
+import 'package:flutter_catalog/core/store.dart';
+import 'package:velocity_x/velocity_x.dart';
+
 import 'catalog.dart';
 
 class CartModel{
@@ -16,10 +19,20 @@ class CartModel{
   List<Item> get item => _itemIds.map((id) => _catalog.getById(id)).toList();
 
   num get totalPrice => item.fold(0, (total, current) => total + current.price);
-  void add(Item item){
-    _itemIds.add(item.id);
-  }
+
   void remove(Item item){
     _itemIds.remove(item.id);
   }
+
+}
+class AddMutation extends VxMutation<MyStore>{
+  final Item item;
+
+  AddMutation(this.item);
+  @override
+  perform() {
+    store.cart._itemIds.add(item.id );
+    throw UnimplementedError();
+  }
+
 }
--- lib/widgets/home_widgets/add_to_cart.dart
+++ lib/widgets/home_widgets/add_to_cart.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/core/store.dart';
 import 'package:flutter_catalog/models/cart.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class AddToCart extends StatelessWidget {
   final Item catalog;
+
   AddToCart({Key key, this.catalog}) : super(key: key);
-  final _cart=CartModel();
 
   @override
   Widget build(BuildContext context) {
+    VxState.listen(context,to: [AddMutation]);
+    final CartModel _cart=  (VxState.store as MyStore).cart;
+
     bool isInCart = _cart.item.contains(catalog) ?? false;
     return ElevatedButton(
       onPressed: () {
         if (!isInCart) {
-          isInCart = isInCart.toggle();
-          final _catalog = CatalogModel();
-          _cart.catalog = _catalog;
-          _cart.add(catalog);
-
+          AddMutation(catalog);
         }
       },
       style: ButtonStyle(
@@ -31,3 +31,4 @@ class AddToCart extends StatelessWidget {
       child: isInCart ? Icon(Icons.done) : Icon(CupertinoIcons.cart_badge_plus),
     );
   }
+}
